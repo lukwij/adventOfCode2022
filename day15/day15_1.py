4d4cdbe82e41477ca3e0d8a5ef3ab0a841f8c28d
lukwij <lukwij@gmail.com>
2023-01-16 19:02:47 +0100

    day15, part1 Less naive solution, not that fast but feasible for patient people.

--- day15/day15_1.py
+++ day15/day15_1.py
@@ -1,6 +1,7 @@
 # day15_1.py
 
 import re
+import logging
 
 
 def get_sensors_and_beacons(filename: str) -> list:
@@ -14,60 +15,42 @@ def get_sensors_and_beacons(filename: str) -> list:
     return sensors_and_beacons
 
 
-def get_coverage(ss_and_bs: list) -> list:
-    master_coverage = []
+def get_coverage_for_row(ss_and_bs: list, row_no: int) -> list:
+    coverage = []
     for sensor, beacon in ss_and_bs:
-        coverage = []
-        print(f"sensor: {sensor}, beacon: {beacon}")
-        beacon_reached = False
-        coordinates_to_check = [sensor]
-        while not beacon_reached:
-            next_to_check = []
-            for position in coordinates_to_check:
-                if position not in coverage:
-                    coverage.append(position)
-                if position == beacon:
-                    beacon_reached = True
-                if not beacon_reached:
-                    for potential_next in find_new_directions(position):
-                        if (potential_next not in coverage) and (potential_next not in next_to_check):
-                            next_to_check.append(potential_next)
-                    coordinates_to_check = next_to_check
-        master_coverage += coverage
-    return master_coverage
+        logging.info(f"sensor: {sensor}, beacon: {beacon}")
+        radius = calculate_radius(sensor, beacon)
+        coverage += range_covered_in_row(sensor, radius, row_no)
+    positions_wo_beacon = list(set(coverage))
+    logging.info(sorted(positions_wo_beacon))
+    for pair in ss_and_bs:
+        try:
+            logging.info(f"Checking if beacon is in given row")
+            positions_wo_beacon.remove(pair[1])
+            logging.info(f"Beacon found in row and removed!")
+        except ValueError:
+            logging.warning(f"Beacon {pair[1]} not in that row")
+    return positions_wo_beacon
 
 
-def find_new_directions(position: tuple) -> list:
-    directions = [(position[0], position[1] - 1), (position[0] + 1, position[1]), (position[0], position[1] + 1),
-                  (position[0] - 1, position[1])]
-    return directions
+def calculate_radius(sensor: tuple, beacon: tuple) -> int:
+    return abs(sensor[0] - beacon[0]) + abs(sensor[1] - beacon[1])
 
 
-def draw_coverage(coverage: list, sensors_and_beacons: list, row_no):
-    min_x = min([position[0] for position in coverage])
-    max_x = max([position[0] for position in coverage])
-    min_y = min([position[1] for position in coverage])
-    max_y = max([position[1] for position in coverage])
-    count = 0
-    for row in range(min_y, max_y + 1):
-        print(f"{row : > 5} ", end="")
-        for place in range(min_x, max_x + 1):
-            if (place, row) in coverage:
-                if (place, row) in [pair[0] for pair in sensors_and_beacons]:
-                    print("S", end="")
-                elif (place, row) in [pair[1] for pair in sensors_and_beacons]:
-                    print("B", end="")
-                else:
-                    print("#", end="")
-                    if row == row_no:
-                        count += 1
-            else:
-                print(".", end="")
-        print()
-    print(f"count: {count}")
+def range_covered_in_row(sensor, radius, row_no) -> list:
+    row_distance_from_sensor = abs(sensor[1] - row_no)
+    radius_less_distance = radius - row_distance_from_sensor
+    if radius_less_distance < 0:
+        return []
+    range_in_row = []
+    for column in range(sensor[0] - radius_less_distance, sensor[0] + radius_less_distance + 1):
+        range_in_row.append((column, row_no))
+    return range_in_row
 
 
 if __name__ == "__main__":
+    row_number = 2_000_000
+    logging.basicConfig(level=logging.INFO)
     my_sb_pairs = get_sensors_and_beacons("input.txt")
-    my_coverage = get_coverage(my_sb_pairs)
-    draw_coverage(my_coverage, my_sb_pairs, row_no=2000000)
+    my_coverage = get_coverage_for_row(my_sb_pairs, row_no=row_number)
+    logging.info(f"Positions without beacon in row {row_number}: {len(my_coverage)}")
